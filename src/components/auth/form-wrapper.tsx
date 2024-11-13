ff9f743bf702994e046ce0cc9d4f4df9070a216a
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-11-12 19:14:12 -0500

    feat: link between signup and login (#44)
    Fixes #43

--- src/components/auth/form-wrapper.tsx
+++ src/components/auth/form-wrapper.tsx
@@ -1,26 +1,36 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 interface FormWrapperProps {
   children: React.ReactNode;
-  intent: string;
+  header: string;
+  description: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  click: string;
 }
 
-export default function FormWrapper({ children, intent }: FormWrapperProps) {
+export default function FormWrapper({ children, header, description, backButtonLabel, backButtonHref, click }: FormWrapperProps) {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">{intent}</CardTitle>
+        <CardTitle className="text-2xl">{header}</CardTitle>
         <CardDescription>
-          Enter your email below to {intent.toLowerCase()}
+          {description}
         </CardDescription>
       </CardHeader>
       <CardContent>{children}</CardContent>
+      <CardFooter>
+          {backButtonLabel} <Button variant="link" asChild><Link href={backButtonHref}>{click}</Link></Button>
+      </CardFooter>
     </Card>
   );
 }
