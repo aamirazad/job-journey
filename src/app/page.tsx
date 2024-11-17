8c585aff2ed9536b5b44d07e23f6407bf0d9879d
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-11-17 17:37:31 -0500

    feat: header with links (#55)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default async function HomePage() {
   const session = await auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         {session ? <p>Hello {session.user?.role} {session.user?.name}</p> : null}
         <Link href="/auth/login">Login</Link>
         <Link href="/auth/signup">Create an account</Link>
       </div>
-    </main>
+    </div>
   );
 }
