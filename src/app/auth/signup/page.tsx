36a330629e9705aaf3349032c689efb2238a1246
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2024-11-22 20:55:27 -0500

    fix: broken sign up margin (#79)

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "@/components/auth/sign-up-form";
 
 export default function SignUp() {
   return (
-    <div className="flex h-screen w-full items-center justify-center px-4">
+    <div className="flex w-full items-center justify-center px-4">
       <SignUpForm />
     </div>
   );
