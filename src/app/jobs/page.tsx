304ec3135be9aae908fee6495ebd8422092178a7
Aamir Azad <aamirmazad@gmail.com>
2024-11-24 15:56:30 -0500

    maybe fix suspense issue

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,5 +1,11 @@
 import JobListings from "@/components/jobs/job-listings";
+import LoadingSpinner from "@/components/loading-spinner";
+import { Suspense } from "react";
 
 export default function FindJobs() {
-  return <JobListings />;
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <JobListings />
+    </Suspense>
+  );
 }
