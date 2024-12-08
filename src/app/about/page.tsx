f9c5da3866e2cd92f18e8e5eb0b4f09c380e08c9
xkcd576 <wyzhu576@gmail.com>
2024-12-08 14:44:45 -0500

    feat: add about page (#113)
    Co-authored-by: aamirazad <aamirmazad@gmail.com>
    Reviewed-on: https://code.aamira.me/aamirazad/job-portal/pulls/113
    Co-authored-by: xkcd576 <wyzhu576@gmail.com>
    Co-committed-by: xkcd576 <wyzhu576@gmail.com>

---
+++ src/app/about/page.tsx
@@ -0,0 +1,60 @@
+export default function AboutPage() {
+  return (
+    <div className="prose lg:prose-lg mx-auto w-full">
+      <h1>About us</h1>
+      <p>
+        Welcome to ApexWorks! This is a platform creted by high school students
+        for students! Our goal is to bridge the gap between career-seeking
+        teenagers and excited employers, making it easier for students to find
+        work they love while simultaneously helping businesses connect with
+        opportunistic young individuals ready to work.
+      </p>
+      <h1> Who we are </h1>
+      <p>
+        {" "}
+        We are Aamir Azad and William Zhu, two high school students passionate
+        about coding, technology, and entrepreneurship. Over the years, we
+        noticed a common reaccurence: many of our classmates search of places to
+        get some work experience, but there were never any easy options for them
+        to apply. On the other hand, local businesses often struggled to reach
+        out to young job seekers to brighten up their organization. That&apos;s when
+        we knew that a solution had to be made. We put our minds together and we
+        are pround of our end product: ApexWorks.
+      </p>
+      <h1> What We Do </h1>
+      <p>
+        {" "}
+        ApexWorks allows students who may be searching for a job to easily
+        connect to an employer to find their dream job. Simply enter in your
+        ideal job, location, or form of work in order to filter through
+        potential postings. We also assist businesses that are seeking for
+        employees by providing them with a straightforward method of seeking out
+        suitable applcants. By leveraging modern front-end frameworks and
+        back-end technologies, we ensure that the web pages that people access
+        have been built using the latest technologies (e.g. TypeScript) in order
+        to provide the most user-friendly experience.{" "}
+      </p>
+      <h4>
+        {" "}
+        For students: Browse through job listings, apply for opportunities, and
+        keep track of your applications - all in one place. We will also send
+        you an email if you have any matches.{" "}
+      </h4>
+      <h4>
+        {" "}
+        For Employers: Easily submit job postings, manage listings, and connect
+        with enthusiastic and reliable student applicants.
+      </h4>
+      <p>
+        {" "}
+        Thank you for visiting ApexWorks! Whether you&apos;re looking for a job
+        or looking to hire, we&apos;re excited to help you link with the right
+        opportunity.
+      </p>
+      <p>
+        If you have any questions, feel free to contact us at{" "}
+        <a href="mailto:aamirmazad@gmail.com">aamirmazadgmail.com</a>.
+      </p>
+    </div>
+  );
+}
