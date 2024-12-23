07a275d113e4d830c5e2085415b0786b73e9c2c9
Aamir Azad <aamirmazad@gmail.com>
2024-12-23 16:02:29 -0500

    feat: posthog analytics (#133)
    Reviewed-on: https://code.aamira.me/aamirazad/job-journey/pulls/133
    Co-authored-by: Aamir Azad <aamirmazad@gmail.com>
    Co-committed-by: Aamir Azad <aamirmazad@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { ourFileRouter } from "@/app/api/uploadthing/core";
 import NextTopLoader from "nextjs-toploader";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { CSPostHogProvider } from "@/app/_analytics/provider";
 
 export const metadata: Metadata = {
   title: "Student Job Portal",
@@ -20,31 +21,33 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={GeistSans.variable}>
-      <body className="relative flex min-h-screen flex-col">
-        <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-        <NextTopLoader color="#f97316" showSpinner={false} />
-        <Header />
-        <div className="absolute left-0 top-0 -z-10 flex h-full w-full flex-col">
-          <div className="w-full flex-1 bg-gradient-to-b from-[#FF499E]/10 to-[#D264B6]/10" />
-          <div className="w-full flex-1 bg-gradient-to-b from-[#D264B6]/10 to-[#A480CF]/10" />
-          <div className="w-full flex-1 bg-gradient-to-b from-[#A480CF]/10 to-[#779BE7]/30" />
-          <div className="w-full flex-1 bg-gradient-to-b from-[#779BE7]/30 to-[#49B6FF]/30" />
-        </div>
-        <main className="mt-16 flex-1 md:mt-8">
-          <NuqsAdapter>{children}</NuqsAdapter>
-        </main>
-        <Toaster />
-        <Footer />
-      </body>
-    </html>
+    <CSPostHogProvider>
+      <html lang="en" className={GeistSans.variable}>
+        <body className="relative flex min-h-screen flex-col">
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          <NextTopLoader color="#f97316" showSpinner={false} />
+          <Header />
+          <div className="absolute left-0 top-0 -z-10 flex h-full w-full flex-col">
+            <div className="w-full flex-1 bg-gradient-to-b from-[#FF499E]/10 to-[#D264B6]/10" />
+            <div className="w-full flex-1 bg-gradient-to-b from-[#D264B6]/10 to-[#A480CF]/10" />
+            <div className="w-full flex-1 bg-gradient-to-b from-[#A480CF]/10 to-[#779BE7]/30" />
+            <div className="w-full flex-1 bg-gradient-to-b from-[#779BE7]/30 to-[#49B6FF]/30" />
+          </div>
+          <main className="mt-16 flex-1 md:mt-8">
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </main>
+          <Toaster />
+          <Footer />
+        </body>
+      </html>
+    </CSPostHogProvider>
   );
 }
